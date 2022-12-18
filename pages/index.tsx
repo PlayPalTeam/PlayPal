24fc48186f5ba79b77293bec0e146c74a9771abe
sagardwivedi <sagar.dw20@gmail.com>
2022-12-18 19:52:02 +0530

    feat: navbar-v1.1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,17 @@
-import  Navbar  from "../components/Navbar";
+import Navbar from "../components/Navbar";
 import Link from "next/link";
 
 const Home = () => {
-  return <div>
-    <Navbar />
-    		<div className="flex h-screen items-center justify-center">
-			<div className="rounded-md bg-green-600 p-2 text-white">
-				<Link href={"/auth/signup"}>Sign Up</Link>
-			</div>
+	return (
+		<div className="flex items-center justify-between">
+			<Navbar />
+			<Link
+				className="rounded-md bg-green-600 p-2 text-white"
+				href={"/auth/signup"}
+			>
+				Sign Up
+			</Link>
 		</div>
-  </div>
- 
+	);
+};
 export default Home;
