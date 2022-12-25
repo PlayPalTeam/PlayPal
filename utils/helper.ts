823bc405d07a50327cc2e436280e27b0edc09519
sagardwivedi <sagar.dw20@gmail.com>
2022-12-25 12:18:55 +0530

    Remove the helper function

--- utils/helper.ts
+++
@@ -1,7 +0,0 @@
-import { supabase } from "./supabase";
-
-export const getRole = async (id: string) => {
-	const role = supabase.from("profiles").select("role").eq("id", id).single();
-
-	return role;
-};
