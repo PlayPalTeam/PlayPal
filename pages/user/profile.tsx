8cf514b43db07acd2163830c24e18a22ce89a1e9
sagardwivedi <sagar.dw20@gmail.com>
2022-12-27 13:34:11 +0530

    FIx fetching of data

--- pages/user/profile.tsx
+++ pages/user/profile.tsx
@@ -1,11 +1,12 @@
-import Navbar from "../../components/Navbar";
-import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { UserProfileSchema, UserProfileType } from "../../types/types";
-import { useUserProfile } from "../../context/UserProfileContext";
-import { useEffect } from "react";
-import { useSession } from "@supabase/auth-helpers-react";
+import { useSupabaseClient, useUser } from "@supabase/auth-helpers-react";
 import Head from "next/head";
+import { useEffect } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { Navbar, Avatar } from "../../components";
+import { useUserProfile } from "../../context/UserProfileContext";
+import { Database } from "../../types/database.types";
+import { FormUIType, UserProfileSchema, UserProfileType } from "../../types/types";
 
 const profilesettings = [
 	{
@@ -18,17 +19,74 @@ const profilesettings = [
 	},
 ];
 
+const FormUI: FormUIType[] = [
+	{
+		label: "Email",
+		name: "email",
+		disabled: true,
+	},
+	{
+		label: "Username",
+		name: "username",
+		type: "text",
+		placeholder: "e.g. Steven@12",
+	},
+	{
+		label: "Full Name",
+		name: "full_name",
+		type: "text",
+		placeholder: "e.g. Steven King",
+	},
+	{
+		label: "Locality",
+		name: "locality",
+		type: "text",
+		placeholder: "e.g. Andheri",
+	},
+];
+
 const UserProfile = () => {
 	const { userProfile } = useUserProfile();
 
-	const session = useSession();
+	const supabase = useSupabaseClient<Database>();
 
-	const { username, full_name } = userProfile;
+	const user = useUser();
 
-	const { register, reset } = useForm<UserProfileType>({
+	const { username, full_name, locality, avatar_url } = userProfile;
+
+	const { register, reset, handleSubmit } = useForm<UserProfileType>({
 		resolver: zodResolver(UserProfileSchema),
-
 	});
+
+	const onSubmit: SubmitHandler<UserProfileType> = async (info) => {
+		const { status } = await supabase
+			.from("profiles")
+			.update({
+				username: info.username,
+				full_name: info.full_name,
+				locality: info.locality,
+				updated_at: new Date().toISOString(),
+			})
+			.eq("id", user.id);
+
+		if (status === 204) {
+			alert("Your data is updated.");
+		}
+	};
+
+	useEffect(() => {
+		let defaultValues = {
+			email: "",
+			username: "",
+			full_name: "",
+			locality: "",
+		};
+		defaultValues.email = user?.email;
+		defaultValues.username = username;
+		defaultValues.full_name = full_name;
+		defaultValues.locality = locality;
+		reset({ ...defaultValues });
+	}, [full_name, locality, reset, user?.email, username]);
 
 	function decide() {
 		console.log("few");
@@ -41,91 +99,59 @@ const UserProfile = () => {
 			</Head>
 			<div className="flex">
 				<Navbar />
-				<div className="flex w-screen gap-10 p-12 ">
-					{/* main 1 */}
-					<div className="grow-[1]">
-						<div className="p-2">
-							{profilesettings.map((data, index) => (
-								<div
-									key={index}
-									className="m-5 flex flex-col items-start rounded-md border border-green-500 p-3 pt-2"
-								>
-									<button onClick={decide}>{data.name}</button>
-									<div className="text-xs">{data.info} </div>
-								</div>
-							))}
-						</div>
+				<section className="flex flex-wrap gap-10 p-12">
+					<div className="grow-[1] p-2">
+						{profilesettings.map((data, index) => (
+							<button
+								key={index}
+								onClick={decide}
+								className="m-5 flex flex-col items-start rounded-md border border-green-500 p-3 pt-2"
+							>
+								<p>{data.name}</p>
+								<p className="text-xs">{data.info} </p>
+							</button>
+						))}
 					</div>
-					{/* main 2 */}
-					<div className="mr-14 grow-[4]">
-						<div className="flex gap-2 rounded-md border border-green-500 p-12 pl-14">
-							<div className="m-auto ml-14 grow-[2] ">
-								<div>Upload a new Photo</div>
-								<div>Image name </div>
-							</div>
-							<div className=" m-auto mr-4 grow-[1]">
-								<button className="ml-3 rounded-lg border border-green-500 p-2 pl-4 pr-4">
-									Update
-								</button>
-							</div>
+					<div className="grow-[4]">
+						<div className="flex rounded-md border border-green-500">
+							<Avatar uid={user?.id} size={100} url={avatar_url} />
 						</div>
 
-						<div className="mt-5 rounded-md border border-green-500 p-12 pt-2">
+						<div className="mt-3 rounded-md border border-green-500 px-8 py-6">
 							<div className="mt-3">
-								<div className="mb-5 text-lg ">
-									Change User Information here
-								</div>
+								<h3 className="text-lg">Your Information</h3>
 							</div>
-							{/* form  */}
-							<form>
-								<div className="flex gap-9 p-4 pl-0">
-									<div className="grow-[1]">
-										<label className="pb-2 text-sm">Email</label>
-										<input
-											className="inputCss"
-											value={session?.user.email}
-											disabled
-										/>
-									</div>
-									<div className="grow-[1]">
-										<label className="pb-2 text-sm">User Name *</label>
-										<div>
+							<form
+								className="mt-10 space-y-6"
+								onSubmit={handleSubmit(onSubmit)}
+							>
+								<div className="grid grid-cols-2 gap-8 max-lg:grid-cols-1">
+									{FormUI.map((form) => (
+										<div key={form.name}>
+											<label className="pb-2 text-sm" htmlFor={form.name}>
+												{form.label}{" "}
+												<span className="font-bold text-red-900">*</span>
+											</label>
 											<input
 												className="inputCss"
-												type="text"
-												placeholder="e.g. steven12"
-												{...register("username")}
+												type={form.type}
+												placeholder={form.placeholder}
+												{...register(form.name)}
+												disabled={form.disabled}
 											/>
 										</div>
-									</div>
+									))}
 								</div>
-
-								<div className="flex gap-9 p-4 pl-0 ">
-									<div className="grow-[1] ">
-										<label className="pb-2 text-sm">Full Name *</label>
-										<input
-											className="inputCss"
-											type="text"
-											placeholder="e.g. Steven King"
-											{...register("full_name")}
-										/>
-									</div>
-									<div className="grow-[1]">
-										<label className="pb-2 text-sm">Locality *</label>
-										<input
-											className="inputCss"
-											type="text"
-											placeholder="e.g. Andheri"
-										/>
-									</div>
-								</div>
-								<button className="mt-3 w-full rounded-md bg-green-400 p-2 text-white duration-300 ease-in hover:bg-green-500">
+								<button
+									type="submit"
+									className="w-full rounded-md bg-green-400 p-2 font-bold text-white duration-300 ease-in hover:bg-green-500"
+								>
 									Update Information
 								</button>
 							</form>
 						</div>
 					</div>
-				</div>
+				</section>
 			</div>
 		</>
 	);
