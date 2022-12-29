34ddc93f38f5d9ffa4d789f23a1e1a47c5b44894
vaibhav <gholapvaibhav724@gmail.com>
2022-12-29 13:35:28 +0530

    turf context v1

--- pages/lister/turf.tsx
+++ pages/lister/turf.tsx
@@ -1,36 +1,15 @@
 import { useSupabaseClient, useUser } from "@supabase/auth-helpers-react";
 import { useEffect, useState } from "react";
+import { useTurfData } from "../../context/TurfContext";
 import { Database } from "../../types/database.types";
 
 const Turfs = () => {
-	const supabase = useSupabaseClient<Database>();
-	const user = useUser();
-	const [data, setData] = useState({});
-
-	useEffect(() => {
-		const getTurfData = async () => {
-			const { data, error } = await supabase
-				.from("profiles")
-				.select(`*, turfs(*)`)
-				.eq("id", user?.id)
-				.single();
-			if (error) {
-				console.log(error.code);
-				console.log(error.hint);
-				console.log(error.message);
-				console.log(error.details);
-			}
-			if (data) {
-				setData(data);
-			}
-		};
-
-		getTurfData();
-	}, [supabase, user?.id]);
+	const { turfData } = useTurfData()
+	console.log(turfData)
 
 	return (
 		<div>
-      {JSON.stringify(data)}
+
 			
 			<form></form>
 		</div>
