7c28fcc12f6540d49ee37e41207af00043a6343d
sagardwivedi <sagar.dw20@gmail.com>
2023-01-01 11:10:36 +0530

    created context for turf and userprofile

--- src/context/TurfContext.tsx
+++ src/context/TurfContext.tsx
@@ -1,27 +1,46 @@
 import { useSupabaseClient, useUser } from "@supabase/auth-helpers-react";
 import {
 	createContext,
-	ReactNode,
-	useContext,
 	useState,
 	useEffect,
+	SetStateAction,
+	Dispatch,
+	useContext,
+	ReactNode,
+	useMemo,
 } from "react";
 import { toast } from "react-hot-toast";
 import { Database } from "../types/database.types";
 
-const TurfContext = createContext({
-	TurfData: {},
+type Turf = Database["public"]["Tables"]["turfs"]["Row"];
+type TurfInsert = Database["public"]["Tables"]["turfs"]["Insert"];
+type TurfUpdate = Database["public"]["Tables"]["turfs"]["Update"];
+
+interface TurfContextType {
+	turfs: Turf | Turf[];
+	setTurfs: Dispatch<SetStateAction<Turf | Turf[]>>;
+	addTurf: (turf: TurfInsert) => Promise<void>;
+	updateTurf: (id: string, update: TurfUpdate) => Promise<void>;
+	deleteTurf: (id: string) => Promise<void>;
+}
+
+export const TurfContext = createContext<TurfContextType>({
+	turfs: [],
+	setTurfs: () => {},
+	addTurf: () => Promise.resolve(),
+	updateTurf: () => Promise.resolve(),
+	deleteTurf: () => Promise.resolve(),
 });
 
 export const TurfProvider = ({ children }: { children: ReactNode }) => {
-	const [TurfData, setTurfData] = useState({});
+	const [turfs, setTurfs] = useState<Turf | Turf[]>([]);
 
 	const supabase = useSupabaseClient<Database>();
 
 	const user = useUser();
 
-	useEffect(() => {
-		const getTurfData = async () => {
+	const getData = useMemo(() => {
+		return async () => {
 			const { data, error } = await supabase
 				.from("turfs")
 				.select("*")
@@ -32,17 +51,60 @@ export const TurfProvider = ({ children }: { children: ReactNode }) => {
 			}
 
 			if (data) {
-				setTurfData(data);
+				setTurfs(data);
 			}
 		};
+	}, [supabase, user?.id]);
+
+	useEffect(() => {
 		if (user) {
-			getTurfData();
+			getData();
 		}
-	}, [supabase, user, user?.id]);
+	}, [getData, user]);
+
+	const addTurf = async (turf: TurfInsert) => {
+		await supabase.from("turfs").insert({ ...turf, profile_id: user.id });
+	};
+
+	const updateTurf = async (id: string, update: TurfUpdate) => {
+		const { status, error } = await supabase
+			.from("turfs")
+			.update(update)
+			.eq("id", id);
+
+		if (error) {
+			toast.error(error.message);
+		}
+
+		if (status === 204) {
+			toast.success(`Update done for ${update.turf_name}`);
+		}
+	};
+
+	const deleteTurf = async (id: string) => {
+		const { error, status } = await supabase
+			.from("turfs")
+			.delete()
+			.eq("turf_id", id);
+
+		if (error) {
+			toast.error(error.message);
+		}
+
+		if (status === 204) {
+			toast.success(`Deleted ${id}`);
+		}
+	};
 
 	return (
-		<TurfContext.Provider value={{ TurfData }}>{children}</TurfContext.Provider>
+		<TurfContext.Provider
+			value={{ turfs, setTurfs, addTurf, updateTurf, deleteTurf }}
+		>
+			{children}
+		</TurfContext.Provider>
 	);
 };
 
-export const useTurfContext = () => useContext(TurfContext);
+export const useTurfContext = () => {
+	return useContext<TurfContextType>(TurfContext);
+};
