8c09ea06d7ebb8b7e6889395f4c365c21f3d884a
sagardwivedi <sagar.dw20@gmail.com>
2023-03-07 01:07:26 +0530

    Add SearchBar and click handler to ListerDashBoard page

--- pages/lister/index.tsx
+++ pages/lister/index.tsx
@@ -1,16 +1,37 @@
+import SearchBar from '@components/Search';
 import { useTurfContext } from '@context/TurfContext';
 import dynamic from 'next/dynamic';
+import { useRouter } from 'next/router';
+import { ChangeEvent, useState, useMemo } from 'react';
 
 const TurfCard = dynamic(() => import('@components/TurfCard'));
-const CardDisclosure = dynamic(() => import('@components/CardDisclosure'));
 
 const ListerDashBoard = () => {
+  const [query, setQuery] = useState('');
   const { turfs } = useTurfContext();
-  const TurfELement = turfs.map((turf) => <TurfCard showBookings key={turf.turf_id} {...turf} />);
+  const router = useRouter();
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+  };
+
+  const handleClick = () => {
+    router.push('/lister/turfs/addTurf');
+  };
+
+  const filteredTurfs = useMemo(() => turfs.filter((turf) => turf.turf_name.toLowerCase().includes(query.toLowerCase())), [turfs, query]);
+
+  const TurfElement = filteredTurfs.map((turf) => <TurfCard href="turfs" key={turf.turf_id} turf={turf} />);
 
   return (
-    <main className="mx-auto max-w-4xl">
-      <CardDisclosure title="Turfs" element={TurfELement} />
+    <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div className="my-8 flex items-center justify-between">
+        <SearchBar query={query} handleChange={handleInputChange} className="mr-4 w-[35%]" />
+        <button className="btn-primary btn" onClick={handleClick}>
+          Add Turf
+        </button>
+      </div>
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">{TurfElement}</div>
     </main>
   );
 };
