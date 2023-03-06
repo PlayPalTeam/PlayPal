5626814d1c69c687761e20a688974322540e49d2
sagardwivedi <sagar.dw20@gmail.com>
2023-03-07 00:46:31 +0530

    Add hook useHelper and update href in TurfCard component

--- src/components/TurfCard.tsx
+++ src/components/TurfCard.tsx
@@ -2,18 +2,21 @@ import Link from 'next/link';
 import { Turf } from 'src/types/types';
 import dynamic from 'next/dynamic';
 import { FaMapMarkerAlt, FaFutbol } from 'react-icons/fa';
+import useHelper from '@hooks/useHelper';
 
 interface TurfCardProps {
   turf: Turf;
+  href: string;
 }
 
 const Avatar = dynamic(() => import('@components/Ava'));
 
-const TurfCard = ({ turf }: TurfCardProps) => {
+const TurfCard = ({ turf, href }: TurfCardProps) => {
+  const { getRoleHref } = useHelper();
   return (
-    <Link className="w-full max-w-xs overflow-hidden rounded-lg shadow-md" href={`/lister/turfs/${turf?.turf_id}`}>
+    <Link className="w-full max-w-xs overflow-hidden rounded-lg shadow-md" href={getRoleHref(`${href}/${turf?.turf_id}`)}>
       <div className="h-48">
-        <Avatar size='400' src={turf?.turf_image} className="h-full w-full" />
+        <Avatar size="400" src={turf?.turf_image} className="h-full w-full" />
       </div>
       <div className="p-4">
         <h3 className="flex items-center text-lg font-semibold">
