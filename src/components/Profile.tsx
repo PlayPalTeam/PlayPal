fce74629235f780eeeece8e01b23b0d0bf03fdbe
sagardwivedi <sagar.dw20@gmail.com>
2023-03-07 00:45:32 +0530

    Update Avatar component to include className and object-contain to improve image quality

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -28,7 +28,7 @@ const Profile = () => {
     <main className="form-control mx-auto w-[90%] max-w-5xl max-md:py-20 md:py-40">
       <div className="flex max-md:flex-col max-md:gap-y-5">
         <section className="md:w-1/2">
-          <Avatar showUploadButton id={userProfile?.id} src={userProfile?.avatar_url} />
+          <Avatar showUploadButton id={userProfile?.id} className="h-1/2 w-1/2 rounded-2xl object-contain" src={userProfile?.avatar_url} />
         </section>
         <section className="space-y-8 md:w-1/2">
           <FormProvider {...method}>
