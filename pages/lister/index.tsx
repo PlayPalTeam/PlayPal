d6eb23fba1b30129ec8c315df2f3f093d4459b8d
sagardwivedi <sagar.dw20@gmail.com>
2023-03-07 00:44:03 +0530

    Update routing in Lister dashboard to redirect to add turf page on button click

--- pages/lister/index.tsx
+++ pages/lister/index.tsx
@@ -16,18 +16,20 @@ const ListerDashBoard = () => {
   };
 
   const handleClick = () => {
-    router.push('/lister/turfs');
+    router.push('/lister/turfs/addTurf');
   };
 
   const filteredTurfs = useMemo(() => turfs.filter((turf) => turf.turf_name.toLowerCase().includes(query.toLowerCase())), [turfs, query]);
 
-  const TurfElement = filteredTurfs.map((turf) => <TurfCard key={turf.turf_id} turf={turf} />);
+  const TurfElement = filteredTurfs.map((turf) => <TurfCard href="turfs" key={turf.turf_id} turf={turf} />);
 
   return (
     <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <div className="my-8 flex items-center justify-between">
         <SearchBar query={query} handleChange={handleInputChange} className="mr-4 w-[35%]" />
-        <button className="btn-primary btn" onClick={handleClick}>Add Turf</button>
+        <button className="btn-primary btn" onClick={handleClick}>
+          Add Turf
+        </button>
       </div>
       <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">{TurfElement}</div>
     </main>
