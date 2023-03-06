b0ea649727729bd1ff5ef122f74b0ee5ed3483fb
sagardwivedi <sagar.dw20@gmail.com>
2023-03-07 01:09:34 +0530

    Commit changes to replace Avatar component with Ava and add id and size attributes to Avatar component in Profile.tsx

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -6,7 +6,7 @@ import { FormProvider, SubmitHandler, useForm } from 'react-hook-form';
 import { ProfileSchema, ProfileType } from 'src/types/types';
 import { useUserProfile } from '@context/UserProfileContext';
 
-const Avatar = dynamic(() => import('@components/Avatar'));
+const Avatar = dynamic(() => import('@components/Ava'));
 const FormTitle = dynamic(() => import('@components/FormElement').then((mod) => mod.FormTitle));
 const FormInput = dynamic(() => import('@components/FormElement').then((mod) => mod.FormInput));
 const Button = dynamic(() => import('@components/Button'));
@@ -28,7 +28,7 @@ const Profile = () => {
     <main className="form-control mx-auto w-[90%] max-w-5xl max-md:py-20 md:py-40">
       <div className="flex max-md:flex-col max-md:gap-y-5">
         <section className="md:w-1/2">
-          <Avatar showUploadButton className="h-48 w-48 rounded-full" size="200" />
+          <Avatar showUploadButton id={userProfile?.id} className="h-1/2 w-1/2 rounded-2xl object-contain" src={userProfile?.avatar_url} />
         </section>
         <section className="space-y-8 md:w-1/2">
           <FormProvider {...method}>
@@ -45,7 +45,7 @@ const Profile = () => {
           </FormProvider>
         </section>
       </div>
-  </main>
+    </main>
   );
 };
 
