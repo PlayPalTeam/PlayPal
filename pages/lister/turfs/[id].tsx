635cf0f15f9f9a2cddc4562d5801ea9a0f417917
sagardwivedi <sagar.dw20@gmail.com>
2023-03-07 00:44:13 +0530

    Update components and content for Turf page

--- pages/lister/turfs/[id].tsx
+++ pages/lister/turfs/[id].tsx
@@ -1,65 +1,73 @@
-import { Calendar, momentLocalizer } from 'react-big-calendar';
-import 'react-big-calendar/lib/css/react-big-calendar.css';
-import moment, { Moment } from 'moment';
-import { utcToZonedTime } from 'date-fns-tz';
-import { GetServerSideProps, GetServerSidePropsContext } from 'next';
-import toast from 'react-hot-toast';
-import { supabase } from '@lib/supabase';
+import { useBookContext } from '@context/BookingContext';
+import { useTurfContext } from '@context/TurfContext';
+import dynamic from 'next/dynamic';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 
-interface FormattedBooking {
-  start: string;
-  end: string;
-  title: string;
-}
+const Avatar = dynamic(() => import('@components/Ava'));
+const BookingCard = dynamic(() => import('@components/BookingCard'));
 
-interface ShowBookingsProps {
-  item: {
-    date: string;
-    times: FormattedBooking[];
-  };
-}
+const Turf = () => {
+  const { query } = useRouter();
+  const { id } = query;
+  const { turfs } = useTurfContext();
+  const { listerbooks } = useBookContext();
 
-const localizer = momentLocalizer(moment);
+  const turf = turfs.find((c) => c.turf_id === id);
+  const books = listerbooks.filter((c) => c.turf_id === id);
 
-export const getServerSideProps: GetServerSideProps<ShowBookingsProps> = async (context: GetServerSidePropsContext) => {
-  const { data, error } = await supabase.from('bookings').select('date, times').eq('turf_id', context.params?.id).single();
+  const BookingCards = books.map((book) => <BookingCard key={book.booking_id} {...book} />);
 
-  if (error) {
-    toast.error(error.message);
-    return {
-      notFound: true
-    };
-  }
-
-  const date = new Date(data?.date as string);
-  const timeZone = 'Asia/Kolkata';
-  const zonedDate = utcToZonedTime(date, timeZone);
-
-  const formatTime = data?.times?.map((time) => {
-    const [start, end] = time.split('-');
-    const startDateFormat = moment(zonedDate)
-      .set({ hour: Number(start.split(':')[0]), minute: Number(start.split(':')[1]) })
-      .toISOString();
-    const endDateFormat = moment(zonedDate)
-      .set({ hour: Number(end.split(':')[0]), minute: Number(end.split(':')[1]) })
-      .toISOString();
-
-    return { start: startDateFormat, end: endDateFormat, title: time };
-  });
-
-  return {
-    props: {
-      item: { date: zonedDate.toISOString(), times: formatTime ?? [] }
-    }
-  };
-};
-
-const ShowBookings = ({ item }: ShowBookingsProps) => {
   return (
-    <div style={{ height: 500 }}>
-      <Calendar localizer={localizer} events={item.times} startAccessor="start" endAccessor="end" />
-    </div>
+    <>
+      <Head>
+        <title>{turf?.turf_name}</title>
+      </Head>
+      <main className="mx-auto mt-5 max-w-7xl px-4 sm:px-6 lg:px-8">
+        <section>
+          <Avatar className="mx-auto mb-8 h-52 w-full rounded-2xl" src={turf?.turf_image} />
+          <h2 className="mb-4 text-3xl font-bold">{turf?.turf_name}</h2>
+          <p className="mb-4 text-lg">{turf?.description}</p>
+          <div className="mb-4 flex flex-wrap">
+            <p className="w-full md:w-1/3">
+              <span className="mr-2 font-bold">Address:</span> {turf?.address}
+            </p>
+            <p className="w-full md:w-1/3">
+              <span className="mr-2 font-bold">Open Hours:</span> {turf?.open_hour} - {turf?.close_hour}
+            </p>
+            <p className="w-full md:w-1/3">
+              <span className="mr-2 font-bold">Price:</span> {turf?.price} <span>&#8377;</span>
+            </p>
+            <p className="w-full md:w-1/3">
+              <span className="mr-2 font-bold">Capacity:</span> {turf?.capacity}
+            </p>
+          </div>
+          <div className="mb-8 flex flex-wrap">
+            <div className="w-full md:w-1/2">
+              <p className="mb-2 font-bold">Amenities:</p>
+              <ul className="list-inside list-disc">
+                {turf?.amenities?.map((amenity) => (
+                  <li key={amenity}>{amenity}</li>
+                ))}
+              </ul>
+            </div>
+            <div className="w-full md:w-1/2">
+              <p className="mb-2 font-bold">Sports:</p>
+              <ul className="list-inside list-disc">
+                {turf?.sports?.map((sport) => (
+                  <li key={sport}>{sport}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </section>
+        <section>
+          <h2>Bookings</h2>
+          <div className="mt-5 space-y-5">{BookingCards}</div>
+        </section>
+      </main>
+    </>
   );
 };
 
-export default ShowBookings;
+export default Turf;
