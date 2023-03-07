038cb805b01410b6a205fcfd707090cf4e2780f7
sagardwivedi <sagar.dw20@gmail.com>
2023-03-07 23:46:10 +0530

    Update Next config with compression, production browser source maps and output type

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@
 const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
-	output: "standalone"
+	output: "standalone",
+	compress: true,
+	productionBrowserSourceMaps: false,
+
 };
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
