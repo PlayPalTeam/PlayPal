194b32f4a46ef16de6856c13e304eb2975c1c809
Sagar Dwivedi <Sagar.dw20@gmail.com>
2023-03-08 11:47:09 +0530

    commit

--- src/hooks/useHelper.tsx
+++ src/hooks/useHelper.tsx
@@ -13,24 +13,23 @@ const useHelper = () => {
     const date = new Date(`2000-01-01T${value}`);
     return format(date, 'h:mm a');
   };
-
   const createOneHourSlot = (startTime: string, endTime: string, times: string[]): Array<{ value: string; label: string; disabled: boolean }> => {
+    const SlotOption = [];
     const start = new Date(`2023-02-18T${startTime}`);
     const end = new Date(`2023-02-18T${endTime}`);
-
-    const SlotOption = [];
-
-    for (let current = start; current <= end; current = addHours(current, 1)) {
-      const endHour = addHours(current, 1);
-      const label = `${format(current, 'h:mm a')} - ${format(endHour, 'h:mm a')}`;
-      const isDisabled = times.includes(label);
+    let current = start;
+    while (current <= end) {
+      const endHour = new Date(current.getTime() + 60 * 60 * 1000).getHours();
+      const label = convertTime(current.toTimeString().slice(0, 8)) + ' - ' + convertTime(endHour + ':00:00');
+      const disabled = times.includes(label);
       SlotOption.push({
         value: label,
         label: label,
-        isDisabled: isDisabled
+        isDisabled: disabled
       });
+      current = new Date(current.getTime() + 60 * 60 * 1000); // add one hour
+      if (current >= end) break; // break if current time is past the end time
     }
-
     return SlotOption;
   };
 
