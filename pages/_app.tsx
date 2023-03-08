8ed25160028d14f64bc8855350f479578da682c0
sagardwivedi <sagar.dw20@gmail.com>
2023-03-09 04:13:12 +0530

    Add LoadingLine component to App component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { AppProps } from 'next/app';
 import { Toaster } from 'react-hot-toast';
 import { Roboto } from 'next/font/google';
@@ -7,19 +6,20 @@ import dynamic from 'next/dynamic';
 import { Session } from '@supabase/auth-helpers-nextjs';
 import '../styles/globals.css';
 
-const inter = Roboto({ weight: '400', subsets: ['latin'] });
-
 const Navbar = dynamic(() => import('@components/Navbar'));
 const Transition = dynamic(() => import('@components/Transition'));
 const Head = dynamic(() => import('@components/Head'));
 const Context = dynamic(() => import('@components/Context'));
+const LoadingLine = dynamic(() => import('@components/LoadingLine'));
 
-function App({
+const inter = Roboto({ weight: '400', subsets: ['latin'] });
+
+const App = ({
   Component,
   pageProps
 }: AppProps<{
   initialSession: Session;
-}>) {
+}>) => {
   const { pathname } = useRouter();
 
   const showNavbar = pathname.includes('user') || pathname === '/community' || pathname.includes('lister') || pathname.includes('moderator');
@@ -29,6 +29,7 @@ function App({
       <Head />
       <Toaster position="top-right" />
       <Context initialSession={pageProps.initialSession}>
+        <LoadingLine />
         {showNavbar && <Navbar />}
         <Transition>
           <Component {...pageProps} className={inter.className} />
@@ -36,6 +37,6 @@ function App({
       </Context>
     </>
   );
-}
+};
 
 export default App;
