878d60eb700221e5bee4d94daa2f6a2d5d63d129
sagardwivedi <sagar.dw20@gmail.com>
2023-03-10 20:21:11 +0530

    Add cost prop and update BookingCard to show total cost and delete button for users

--- src/components/BookingCard.tsx
+++ src/components/BookingCard.tsx
@@ -2,17 +2,18 @@ import { Booking, useBookContext } from '@context/BookingContext';
 import { BsCalendarDate } from 'react-icons/bs';
 import { BiTime } from 'react-icons/bi';
 import { ImLocation } from 'react-icons/im';
-import { memo } from 'react';
+import { memo, useCallback } from 'react';
+import { useUserProfile } from '@context/UserProfileContext';
 
-const BookingCard = ({ date, turfs, times, booking_id }: Booking) => {
+const BookingCard = ({ date, turfs, times, booking_id, cost }: Booking) => {
   const { deleteBooking } = useBookContext();
+  const { userProfile } = useUserProfile();
 
   const handleDelete = () => {
     deleteBooking(booking_id);
   };
 
-  // Calculate the total cost of the booking
-  const totalCost = Array.isArray(turfs) ? turfs.reduce((total, turf) => total + turf.price * times.length, 0) : turfs.price * times.length;
+  const turfList = useCallback(() => (Array.isArray(turfs) ? turfs : [turfs]), [turfs]);
 
   return (
     <div className="card bg-neutral text-neutral-content">
@@ -26,22 +27,21 @@ const BookingCard = ({ date, turfs, times, booking_id }: Booking) => {
           {times}
         </div>
         <div className="mt-4">
-          {Array.isArray(turfs) ? (
-            turfs.map((turf) => (
-              <div key={turf?.turf_name} className="">
-                {turf?.turf_name} - <ImLocation /> {turf?.address}
-              </div>
-            ))
-          ) : (
-            <div className="flex items-center gap-x-4">
-              {turfs?.turf_name} - <ImLocation /> {turfs?.address}
+          {turfList().map((turf) => (
+            <div key={turf?.turf_name} className="flex items-center">
+              {turf?.turf_name} <ImLocation className="ml-4 mr-2" />
+              {turf?.address}
             </div>
+          ))}
+          <div className="mt-4 text-lg font-medium">Total cost: &#8377;{cost}</div>
+          {userProfile?.role === 'user' && (
+            <>
+              <hr className="my-5" />
+              <button onClick={handleDelete} type="button" className="btn-outline btn-error btn">
+                Delete
+              </button>
+            </>
           )}
-          <div className="mt-4 text-lg font-medium">Total cost: ${totalCost}</div>
-          <hr className="my-5" />
-          <button onClick={handleDelete} type="button" className="btn-outline btn-error btn">
-            Delete
-          </button>
         </div>
       </div>
     </div>
