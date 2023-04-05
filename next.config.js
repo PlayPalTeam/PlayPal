496da8c91c9bba2f502e4231ac95564424e4cc98
sagardwivedi <sagar.dw20@gmail.com>
2023-04-05 22:48:26 +0530

    Fixed some bugs and Ui improvement

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	swcMinify: true
-
+	swcMinify: true,
+	output: "standalone",
+	compress: true,
 };
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
