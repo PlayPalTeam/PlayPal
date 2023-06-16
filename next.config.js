a0aa7888c94ee3aa474b20519914b9ba7bbd8d81
sagardwivedi <sagar.dw20@gmail.com>
2023-06-16 16:18:01 +0530

    Add environment variables, enable serverActions and update bundle-analyzer in next.config.js file
    This commit adds environment variables to the next.config.js file by pulling values from process env. It also enables serverActions under the experimental property and removes unnecessary whitespaces. Finally, it updates the bundle-analyzer package configuration by removing the colon in the enabled property.

--- next.config.js
+++ next.config.js
@@ -1,15 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	swcMinify: true,
-	images: {
-		domains: ["source.unsplash.com"],
-	  },
-	
+  reactStrictMode: true,
+  swcMinify: true,
+  env: {
+    SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    SUPBASE_ANON: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  },
+  images: {
+    domains: ['source.unsplash.com']
+  },
+  experimental: {
+    serverActions: true
+  }
 };
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
-	enabled: process.env.ANALYZE === 'true',
-})
+  enabled: process.env.ANALYZE === 'true'
+});
 
-module.exports = withBundleAnalyzer(nextConfig)
+module.exports = withBundleAnalyzer(nextConfig);
