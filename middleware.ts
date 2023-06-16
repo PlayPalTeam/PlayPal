ca0927208cecf6a133c4a4ac63db1c1d2f51fd1a
sagardwivedi <sagar.dw20@gmail.com>
2023-06-16 16:17:24 +0530

    Change middleware.ts to use NextResponse async constructor. Add Database type to supabase client.

--- middleware.ts
+++ middleware.ts
@@ -1,42 +1,12 @@
 import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
+
+import type { NextRequest } from 'next/server';
+import { Database } from 'src/types/database.types';
 
 export async function middleware(req: NextRequest) {
-  // We need to create a respmdonse and hand it to the supabase client to be able to modify the response headers.
-
   const res = NextResponse.next();
-
-  const redirectUrl = req.nextUrl.clone();
-
-  // Create authenticated Supabase Client.
-  const supabase = createMiddlewareClient({ req, res });
-  // Check if we have a session
-  const {
-    data: { session }
-  } = await supabase.auth.getSession();
-
-  if (!session) {
-    redirectUrl.pathname = '/auth/signin';
-    return NextResponse.redirect(redirectUrl);
-  }
-
-  if (session && session?.user.user_metadata.role === 'user' && req.nextUrl.pathname.startsWith('/user')) {
-    return res;
-  }
-
-  if (session && session?.user.user_metadata.role === 'lister' && req.nextUrl.pathname.startsWith('/lister')) {
-    return res;
-  }
-
-  if (session && req.nextUrl.pathname.startsWith('/moderator')) {
-    return res;
-  }
-
-  // Auth condition not met, redirect to home page.
-  redirectUrl.pathname = '/auth/signin';
-  return NextResponse.redirect(redirectUrl);
+  const supabase = createMiddlewareClient<Database>({ req, res });
+  await supabase.auth.getSession();
+  return res;
 }
-
-export const config = {
-  matcher: ['/user/:path*', '/lister/:path*', '/moderator/']
-};
