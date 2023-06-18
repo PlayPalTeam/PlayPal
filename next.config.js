b9f14ea402e9b85473941ec00a76edb4ca7d1318
sagardwivedi <sagar.dw20@gmail.com>
2023-06-18 18:01:11 +0530

    Update next.config.js
    Removed unused code and modified 'reactStrictMode' property in next.config.js.

--- next.config.js
+++ next.config.js
@@ -1,21 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  env: {
-    SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    SUPBASE_ANON: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-  },
-  images: {
-    domains: ['source.unsplash.com']
-  },
-  experimental: {
-    serverActions: true
-  }
+    reactStrictMode:true
 };
-
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true'
-});
-
-module.exports = withBundleAnalyzer(nextConfig);
