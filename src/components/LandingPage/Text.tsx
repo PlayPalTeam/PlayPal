f0d2c6a9d11e5be775aacfabc5f7d01b9aa8d7bc
sagardwivedi <sagar.dw20@gmail.com>
2023-06-18 15:59:20 +0530

    Add text to the landing page hero component Adds text to the landing page hero component, including a heading and three paragraphs. The headings and paragraphs make it clear what the PlayPal platform is and what it offers. The changes alter the text font and styles to match the rest of the page. New icons are also added to the page.

---
+++ src/components/LandingPage/Text.tsx
@@ -0,0 +1,54 @@
+import Flower from '@/components/Icons/Flower';
+import HourGlass from '@/components/Icons/HourGlass';
+import { Staatliches } from 'next/font/google';
+import { manrope } from '@/components/LandingPage/Hero';
+
+const staatliches = Staatliches({
+  weight: ['400'],
+  style: 'normal',
+  fallback: ['sans-serif'],
+  display: 'auto',
+  subsets: ['latin']
+});
+
+export default function Text() {
+  return (
+    <div
+      className={`relative flex h-min w-full flex-col flex-nowrap content-center items-center justify-start overflow-hidden bg-[#004b23] p-[100px]`}
+    >
+      <div className="relative z-10 flex h-min w-full flex-1 flex-shrink-0 flex-row flex-nowrap content-center items-center justify-between overflow-hidden">
+        <div className="relative flex h-min w-full max-w-[600px] flex-1 flex-shrink-0 flex-col flex-nowrap content-start items-start justify-center gap-[40px] ">
+          <h2
+            className={`relative h-auto w-full flex-shrink-0 overflow-visible whitespace-pre-wrap break-words text-5xl leading-[1.2] ${staatliches.className} text-[#70e000]`}
+          >
+            Elevate Your Game with PlayPal
+          </h2>
+          <h3
+            className={`${manrope.className} h-auto w-full flex-1 flex-shrink-0 overflow-visible whitespace-pre-wrap break-words text-xl leading-6 tracking-tight text-white`}
+          >
+            Welcome to PlayPal, where the world of turf booking and listing is transformed into a
+            seamless experience. Our platform connects players, turf owners, and the love for a good
+            game.
+          </h3>
+          <h3
+            className={`${manrope.className} h-auto w-full flex-1 flex-shrink-0 overflow-visible whitespace-pre-wrap break-words text-xl leading-6 tracking-tight text-white`}
+          >
+            Discover your next battleground with our ever-growing list of turfs in various
+            locations, suited for multiple sports. Create requests for other players and dive into
+            adrenaline-pumping gaming action.
+          </h3>
+          <h3
+            className={`${manrope.className} h-auto w-full flex-1 flex-shrink-0 overflow-visible whitespace-pre-wrap break-words text-xl leading-6 tracking-tight text-white`}
+          >
+            Don’t settle for less – PlayPal makes organizing and participating in matches a breeze.
+            It’s time to unleash the player within you!
+          </h3>
+        </div>
+        <div className="relative flex h-min w-min flex-shrink-0 flex-col flex-nowrap content-center items-center justify-center gap-[40px] overflow-hidden">
+          <HourGlass />
+          <Flower />
+        </div>
+      </div>
+    </div>
+  );
+}
