d980f3d6fa1b081abbe4f2c9aec8cbe162c4110b
sagardwivedi <sagar.dw20@gmail.com>
2023-06-18 18:03:09 +0530

    Add Gallery Component to Landing Page
    Added a new Gallery component to the Landing Page which displays "TURF INSPIRATION" centered on a green background.

---
+++ src/components/LandingPage/Gallery.tsx
@@ -0,0 +1,10 @@
+export default function Gallery() {
+  return (
+    <div className="p-100 relative flex h-min w-full flex-shrink-0 flex-col flex-nowrap content-center items-center justify-start gap-[60] overflow-hidden bg-[#004b23]">
+      <h2 className="relative h-auto max-w-5xl flex-1 flex-shrink-0 overflow-visible whitespace-pre-wrap break-words text-left font-staat text-5xl leading-default text-white">
+        TURF INSPIRATION
+      </h2>
+      <div></div>
+    </div>
+  );
+}
