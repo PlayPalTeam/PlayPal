60133818c9422dc034bc5584ec10695ffbcc4b85
sagardwivedi <sagar.dw20@gmail.com>
2023-06-18 15:58:36 +0530

    Remove multiple files and configuration related to auth and supabase integration.

--- pages/lister/profile.tsx
+++
@@ -1,9 +0,0 @@
-import dynamic from 'next/dynamic';
-
-const Profile = dynamic(() => import('@/components/Profile'));
-
-const listerprofile = () => {
-  return <Profile />;
-};
-
-export default listerprofile;
