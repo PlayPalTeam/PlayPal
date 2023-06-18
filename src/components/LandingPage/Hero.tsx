7025016615815fd968f8a554379c1cd65eb95f4c
sagardwivedi <sagar.dw20@gmail.com>
2023-06-18 18:03:20 +0530

    Improve font import in Hero component
    Font was imported in an unusual way and caused some issues. We updated it and now everything is clean.

--- src/components/LandingPage/Hero.tsx
+++ src/components/LandingPage/Hero.tsx
@@ -1,20 +1,12 @@
 import { Circle } from '@/components/Icons/Circle';
 import { Logo } from '@/components/Icons/Logo';
 import { Star } from '@/components/Icons/Start';
-import { Manrope } from 'next/font/google';
-
-export const manrope = Manrope({
-  weight: ['700'],
-  style: 'normal',
-  fallback: ['sans-serif'],
-  subsets: ['latin']
-});
 
 export default function Hero() {
   return (
     <header className="flex h-min w-full flex-shrink-0 flex-col flex-nowrap content-center items-center justify-center gap-[20px] overflow-hidden bg-[#006400] px-5 pb-5 pt-[60px] md:justify-start md:gap-[100px] md:p-[100px]">
       <nav
-        className={`flex h-min w-full flex-1 flex-shrink-0 flex-col flex-nowrap content-center items-center justify-center gap-[20px] overflow-hidden text-white md:flex-row ${manrope.className}`}
+        className={`flex h-min w-full flex-1 flex-shrink-0 flex-col flex-nowrap content-center items-center justify-center gap-[20px] overflow-hidden font-manrope text-white md:flex-row`}
       >
         <Logo />
         <p className="flex-1">PlayPal</p>
