4c05a34017332064cbc38f9f3fb0cf39d5245e18
sagardwivedi <sagar.dw20@gmail.com>
2023-06-18 18:01:54 +0530

    Add google fonts and classes to body
    In layout.tsx, add two google fonts to the import section and create classes for them. Add the classes to the body element in the return statement.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,29 @@
 import { ReactNode } from 'react';
 import './globals.css';
 
+import { Manrope, Staatliches } from 'next/font/google';
+
+const manrope = Manrope({
+  weight: ['700'],
+  style: 'normal',
+  fallback: ['sans-serif'],
+  subsets: ['latin'],
+  variable: '--manrope'
+});
+
+const staatliches = Staatliches({
+  weight: ['400'],
+  style: 'normal',
+  fallback: ['sans-serif'],
+  display: 'auto',
+  subsets: ['latin'],
+  variable: '--staatliches'
+});
+
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${manrope.variable} ${staatliches.variable}`}>{children}</body>
     </html>
   );
 }
