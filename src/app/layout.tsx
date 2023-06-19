d7ac5409fd053c3310e64a46a19a033133a3d592
sagardwivedi <sagar.dw20@gmail.com>
2023-06-19 18:38:41 +0530

    style: Reformat code and spacing in layout.tsx
    Fixed multiple style issues in `layout.tsx` related to quotes, spacing, and curly braces. No functional changes were made.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,31 @@
-import { ReactNode } from 'react';
-import './globals.css';
+import { ReactNode } from "react";
+import "./globals.css";
 
-import { Manrope, Staatliches } from 'next/font/google';
+import { Manrope, Staatliches } from "next/font/google";
 
 const manrope = Manrope({
-  weight: ['700', "500"],
-  style: 'normal',
-  fallback: ['sans-serif'],
-  subsets: ['latin'],
-  variable: '--manrope'
+  weight: ["700", "500"],
+  style: "normal",
+  fallback: ["sans-serif"],
+  subsets: ["latin"],
+  variable: "--manrope",
 });
 
 const staatliches = Staatliches({
-  weight: ['400'],
-  style: 'normal',
-  fallback: ['sans-serif'],
-  display: 'auto',
-  subsets: ['latin'],
-  variable: '--staatliches'
+  weight: ["400"],
+  style: "normal",
+  fallback: ["sans-serif"],
+  display: "auto",
+  subsets: ["latin"],
+  variable: "--staatliches",
 });
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${manrope.variable} ${staatliches.variable}`}>{children}</body>
+      <body className={`${manrope.variable} ${staatliches.variable}`}>
+        {children}
+      </body>
     </html>
   );
 }
