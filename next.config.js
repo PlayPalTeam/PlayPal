a282905ad56f021c8f9eb726b5a33d27a5cc31c6
sagardwivedi <sagar.dw20@gmail.com>
2023-06-19 11:15:51 +0530

    Add serverRuntimeConfig to NextConfig
    Added serverRuntimeConfig to NextConfig in next.config.js to hold environment variables with `supabaseUrl` and `supabaseAnonKey`.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode:true
+  reactStrictMode: true,
+  serverRuntimeConfig: {
+    supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    supabaseAnonKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  }
 };
+
