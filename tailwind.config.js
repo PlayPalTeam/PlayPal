150085fc22ccf7a14bef549b34cf3c9c0b812d8a
sagardwivedi <sagar.dw20@gmail.com>
2023-06-19 18:38:31 +0530

    Adjust content property to include more directories on order to properly load css from the expanded src folder.
    Update TailwindCSS config in `tailwind.config.js` to include additional directories for content property, allowing for Tailwind to correctly scan for CSS classes in the expanded `src` directory.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {
       fontFamily: {
-        staat: ['var(--staatliches)'],
-        manrope: ['var(--manrope)']
+        staat: ["var(--staatliches)"],
+        manrope: ["var(--manrope)"],
       },
       lineHeight: {
-        default: '1.2'
+        default: "1.2",
       },
       aspectRatio: {
-        custom: '0.98/1'
-      }
-    }
-  }
+        custom: "0.98/1",
+      },
+    },
+  },
+  plugins: [],
 };
