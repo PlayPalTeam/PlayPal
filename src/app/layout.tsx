e127e62af20436546466ac2d5c8658f3eeed3f72
sagardwivedi <sagar.dw20@gmail.com>
2023-06-21 12:58:28 +0530

    Refactor font imports & add ThemeChanger
    These changes refactor the font imports and add ThemeChanger component to the RootLayout component by adding it as a child of the Providers component.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,35 @@
-import { ReactNode } from "react";
-import "./globals.css";
+import { ReactNode } from 'react';
+import './globals.css';
 
-import { Manrope, Staatliches } from "next/font/google";
+import ThemeSwitch from '@/components/ThemeChanger';
+import { Manrope, Staatliches } from 'next/font/google';
+import { Providers } from './providers';
 
 const manrope = Manrope({
-  weight: ["700", "500"],
-  style: "normal",
-  fallback: ["sans-serif"],
-  subsets: ["latin"],
-  variable: "--manrope",
+  weight: ['700', '500'],
+  style: 'normal',
+  fallback: ['sans-serif'],
+  subsets: ['latin'],
+  variable: '--manrope'
 });
 
 const staatliches = Staatliches({
-  weight: ["400"],
-  style: "normal",
-  fallback: ["sans-serif"],
-  display: "auto",
-  subsets: ["latin"],
-  variable: "--staatliches",
+  weight: ['400'],
+  style: 'normal',
+  fallback: ['sans-serif'],
+  display: 'auto',
+  subsets: ['latin'],
+  variable: '--staatliches'
 });
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${manrope.variable} ${staatliches.variable}`}>
-        {children}
+        <Providers>
+          <ThemeSwitch />
+          {children}
+        </Providers>
       </body>
     </html>
   );
