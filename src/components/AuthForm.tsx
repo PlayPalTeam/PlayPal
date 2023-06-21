8109d312be5fd47535bd203243922db2f94178f9
sagardwivedi <sagar.dw20@gmail.com>
2023-06-21 18:17:38 +0530

    refactor: Modify AuthForm component
    This commit modifies the AuthForm component by changing the username field to email, adding new functions for authentication using supabase and routing to a new page on successful sign-in or sign-up. Additionally, a new FormMessage component was added to the form.

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -1,42 +1,68 @@
 'use client';
 
+import type { Database } from '@/app/database.types';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import { useRouter } from 'next/navigation';
 import { useForm } from 'react-hook-form';
 import * as z from 'zod';
 import { Button } from './ui/button';
-import { Form, FormControl, FormField, FormItem, FormLabel } from './ui/form';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from './ui/form';
 import { Input } from './ui/input';
 
 const formSchema = z.object({
-  username: z.string().min(2).max(10),
+  email: z.string(),
   password: z.string().min(8).max(10)
 });
 
 const AuthForm = () => {
+  const router = useRouter();
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       password: '',
-      username: ''
+      email: ''
     }
   });
+  const supabase = createClientComponentClient<Database>();
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values);
+  async function handleSignIn(values: z.infer<typeof formSchema>) {
+    await supabase.auth.signInWithPassword({
+      email: values.email,
+      password: values.password
+    });
+    form.reset();
+    router.push("/book/dashboard")
+  }
+
+  async function handleSignUp(values: z.infer<typeof formSchema>) {
+    await supabase.auth.signUp({
+      email: values.email,
+      password: values.password,
+      options: {
+        emailRedirectTo: `${location.origin}/auth/callback`
+      }
+    });
+    form.reset();
+    router.refresh();
   }
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 max-w-md mx-auto mt-[10%]">
+      <form
+        onSubmit={form.handleSubmit(handleSignIn)}
+        className="space-y-8 max-w-md mx-auto mt-[10%]"
+      >
         <FormField
           control={form.control}
-          name="username"
+          name="email"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Username</FormLabel>
+              <FormLabel>Email</FormLabel>
               <FormControl>
                 <Input placeholder="shadcn" {...field} />
               </FormControl>
+              <FormMessage />
             </FormItem>
           )}
         />
