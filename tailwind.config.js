faa90efc0470cdb3e0e596864a27cbcf77d6bc83
sagardwivedi <sagar.dw20@gmail.com>
2023-06-23 09:36:28 +0530

    Consolidate Tailwind styles and adjust primary colors
    Changes:
    
    - Consolidated the locations for Tailwind content.
    - Adjusted the primary and secondary background colors.
    - Adjusted primary and secondary text colors.
    - Added new `FontFamily` settings.
    - The `lineHeight` setting has been adjusted.
    - Adjusted the `letterSpacing` setting to have no space.
    - Changed the `borderColor` for primary colors.
    - Added a new `boxShadow` setting of pricing-card-shadow.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,71 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ['class'],
-  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: [
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
-    container: {
-      center: true,
-      padding: '2rem',
-      screens: {
-        '2xl': '1400px'
-      }
-    },
     extend: {
+      backgroundColor: {
+        primary: "#30343f",
+        secondary: "#081c15",
+      },
       colors: {
-        border: 'hsl(var(--border))',
-        input: 'hsl(var(--input))',
-        ring: 'hsl(var(--ring))',
-        background: 'hsl(var(--background))',
-        foreground: 'hsl(var(--foreground))',
-        primary: {
-          DEFAULT: 'hsl(var(--primary))',
-          foreground: 'hsl(var(--primary-foreground))'
-        },
-        secondary: {
-          DEFAULT: 'hsl(var(--secondary))',
-          foreground: 'hsl(var(--secondary-foreground))'
-        },
-        destructive: {
-          DEFAULT: 'hsl(var(--destructive))',
-          foreground: 'hsl(var(--destructive-foreground))'
-        },
-        muted: {
-          DEFAULT: 'hsl(var(--muted))',
-          foreground: 'hsl(var(--muted-foreground))'
-        },
-        accent: {
-          DEFAULT: 'hsl(var(--accent))',
-          foreground: 'hsl(var(--accent-foreground))'
-        },
-        popover: {
-          DEFAULT: 'hsl(var(--popover))',
-          foreground: 'hsl(var(--popover-foreground))'
-        },
-        card: {
-          DEFAULT: 'hsl(var(--card))',
-          foreground: 'hsl(var(--card-foreground))'
-        }
+        primary: "#d8f3dc",
+        secondary: "#1b4332",
       },
-      borderRadius: {
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)'
+      fontFamily: {
+        margarine: ["var(--margarine)"],
+        instrument_sans: ["var(--instrument_sans)"],
       },
-      keyframes: {
-        'accordion-down': {
-          from: { height: 0 },
-          to: { height: 'var(--radix-accordion-content-height)' }
-        },
-        'accordion-up': {
-          from: { height: 'var(--radix-accordion-content-height)' },
-          to: { height: 0 }
-        }
+      lineHeight: {
+        1.1: "1.1",
+        1.2: "1.2",
       },
-      animation: {
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out'
-      }
-    }
+      letterSpacing: {
+        none: "0em",
+      },
+      borderColor: {
+        primary: "#E4D9FF",
+      },
+      boxShadow: {
+        "pricing-card-shadow": "0px 10px 20px -20px rgba(0, 0, 0, 0.25)",
+      },
+    },
   },
-  plugins: [require('tailwindcss-animate')]
+  plugins: [],
 };
